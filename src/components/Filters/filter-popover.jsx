a2bbd6e03702e46aec13e5bdae5468e9b5d9d467
rmaiafrontend <rmaiafrontend@gmail.com>
2024-11-09 22:30:43 -0300

    Atualizações

--- src/components/Filters/filter-popover.jsx
+++ src/components/Filters/filter-popover.jsx
@@ -3,13 +3,13 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 export function FilterPopover({ label, value, children }) {
   return (
     <Popover>
-      <PopoverTrigger className="w-full">
+      <PopoverTrigger className="w-full ">
         <div className="flex flex-col justify-start text-left w-full">
           <p className="font-medium text-[14px] text-left">{label}</p>
           <span className="text-[14px]">{value}</span>
         </div>
       </PopoverTrigger>
-      <PopoverContent className="mt-5 w-auto h-auto">{children}</PopoverContent>
+      <PopoverContent className="mt-4 w-auto h-auto ml-4">{children}</PopoverContent>
     </Popover>
   );
 }
